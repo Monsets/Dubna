efd454aaf95ecd1ce2f0c5b409a0bf9555a3cd91
monset <monset008@gmail.com>
2019-07-08 22:06:41 +0300

    Initial commit

---
+++ cv2open.py
@@ -0,0 +1,94 @@
+from imutils.video import VideoStream
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
+x_min = 279
+y_max = 518
+x_max = 517
+y_min = 320
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
+
+# if the video argument is None, then we are reading from webcam
+if args.get("video", None) is None:
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+
+# otherwise, we are reading from a video file
+else:
+    vs = cv2.VideoCapture(args["video"])
+
+# initialize the first frame in the video stream
+firstFrame = None
+
+# loop over the frames of the video
+while True:
+    # grab the current frame and initialize the occupied/unoccupied
+    # text
+    frame = vs.read()
+    frame = frame if args.get("video", None) is None else frame[1]
+    text = "Unoccupied"
+
+    # if the frame could not be grabbed, then we have reached the end
+    # of the video
+    if frame is None:
+        break
+
+    # resize the frame, convert it to grayscale, and blur it
+    frame = frame[y_min:y_max, x_min: x_max]
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+
+    # compute the absolute difference between the current frame and
+    # first frame
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    # dilate the thresholded image to fill in holes, then find contours
+    # on thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
+
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Occupied"
+
+    # show the frame and record if the user presses a key
+    cv2.imshow("Thresh", thresh)
+    cv2.imshow("Frame Delta", frameDelta)
+    cv2.imshow("Security Feed", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    firstFrame = gray
+
+    time.sleep(.5)
+
+    # if the `q` key is pressed, break from the lop
+
+    if key == ord("q"):
+        break
+
+# cleanup the camera and close any open windows
+vs.stop() if args.get("video", None) is None else vs.release()
+cv2.destroyAllWindows()
