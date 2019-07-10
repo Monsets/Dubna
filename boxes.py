877fbd27a5556e58359781a5db929f7b94a1395f
Nikita Bulakhov <bulahov26@gmail.com>
2019-07-10 14:48:03 +0300

    add reading from JSON

--- boxes.py
+++ boxes.py
@@ -1,5 +1,5 @@
-lower_box = {'start': (28, 115),
+lower_box = {'start': (10, 115),
              'end': (183, 184)}
 
-upper_box = {'start': (31, 0),
+upper_box = {'start': (15, 0),
              'end': (192, 53)}
--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ def construct_arguments():
     ap.add_argument("-v", "--video", help="path to the video file")
     ap.add_argument("-a", "--min-area", type=int, default=13000, help="minimum area size")
     ap.add_argument("-d", "--debugging", type=bool, default=True, help="debugging mode")
+    ap.add_argument("-t", "--test", type=str, default=None,help="testing mode")  # Прописываем аргумент -t и название файла JSON
+    ap.add_argument("-skip", "--skip", type=int, default=0,help="skip n videos")  # Можно скипнуть некоторое количество видосов
     args = vars(ap.parse_args())
 
     return args
